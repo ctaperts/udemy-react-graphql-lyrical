df2bfc74d8a193bab65ab5b9b1e345615bf61c68
Colby Taperts <colbytaperts@gmail.com>
2018-07-08 22:22:00 -0500

    init apollo

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://ctaperts:ctaperts1@ds129831.mlab.com:29831/lyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
